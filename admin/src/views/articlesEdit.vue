d6befab18fe160e45684e3f63112c1bad5dc7d08
waynewhat <980240269@qq.com>
2020-06-18 15:12:39 +0800

    p19

--- admin/src/views/articlesEdit.vue
+++ admin/src/views/articlesEdit.vue
@@ -2,7 +2,7 @@
   <div class="about">
     <h1>{{id?'编辑':'新建'}}文章</h1>
     <el-form label-width="120px" @submit.native.prevent="save">
-       <el-form-item label="所属分类">
+       <el-form-item label="所属分类1">
         <el-select v-model="model.categories" multiple>
           <el-option v-for="item in categories" :key="item._id" :label="item.name" :value="item._id"></el-option>
         </el-select>
